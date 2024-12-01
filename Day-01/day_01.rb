56af11f8a985ae2ee40196d2775d09fdcf865c18
PanoramicPanda <uriah.h.brown@gmail.com>
2024-12-01 01:39:24 -0500

    Day 01

---
+++ Day-01/day_01.rb
@@ -0,0 +1,81 @@
+# Day 1 - Historian Hysteria
+#
+# This class provides methods to calculate distances and similarity scores between elements of two arrays.
+class HistorianHysteria
+  attr_accessor :array_1, :array_2
+
+  # Initializes a new instance of HistorianHysteria.
+  def initialize
+    @array_1 = []
+    @array_2 = []
+    @distances = []
+    @similarity_scores = []
+  end
+
+  # Calculates the absolute distances between numbers at the same positions in two arrays.
+  # @return [nil]
+  def calculate_distances
+    check_arrays
+    sorted_array_1 = @array_1.sort
+    sorted_array_2 = @array_2.sort
+    sorted_array_1.each_with_index do |location, index|
+      @distances << (location - sorted_array_2[index]).abs
+    end
+  end
+
+  # Calculates how many times a number in array one shows up in array two, and then multiplies it by the count.
+  # @return [nil]
+  def calculate_similarity_scores
+    check_arrays
+    @array_1.each do |location|
+      count = @array_2.count(location)
+      @similarity_scores << location * count
+    end
+  end
+
+  # Calculate the distances and return the sum.
+  # @return [Integer] The total distance
+  def get_total_distance
+    calculate_distances
+    @distances.sum
+  end
+
+  # Calculate the similarity scores and return the sum.
+  # @return [Integer] The similarity score
+  def get_similarity_score
+    calculate_similarity_scores
+    @similarity_scores.sum
+  end
+
+  # Loads the input file
+  # @param input_file [String] The path to the input file.
+  # @return [nil]
+  def load_input(input_file)
+    file = File.open(input_file, "r")
+    file.readlines.each do |line|
+      if line.length > 2
+        split_line = line.split(" ")
+        @array_1 << split_line[0].to_i
+        @array_2 << split_line[1].to_i
+      end
+    end
+  end
+
+  # Checks that the arrays are the same length to make sure we formatted them correctly.
+  # Raises an error if they are not.
+  # @raise [RuntimeError] If the arrays are not the same length.
+  # @return [nil]
+  def check_arrays
+    if @array_1.length != @array_2.length
+      Logger.raise 'Arrays are not the same length!'
+    end
+  end
+end
+
+# Example Usage
+if __FILE__ == $0
+  solver = HistorianHysteria.new
+  solver.load_input(File.expand_path('input.txt', __dir__))
+  puts "The total distance is: #{solver.get_total_distance}"
+  puts "The similarity score is: #{solver.get_similarity_score}"
+end
---
+++ spec/day_01_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../Day-01/day_01'
+
+RSpec.describe HistorianHysteria do
+  solver = HistorianHysteria.new
+  solver.array_1 = [3,4,2,1,3,3]
+  solver.array_2 = [4,3,5,3,9,3]
+
+  it 'finds the distances correctly' do
+    expect(solver.get_total_distance).to eq(11)
+  end
+
+  it 'calculates the similarity score correctly' do
+    expect(solver.get_similarity_score).to eq(31)
+  end
+
+end
