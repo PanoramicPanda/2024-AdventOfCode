cda9ce24acd5d63e9178aa7883ca03dbe67fda7e
PanoramicPanda <uriah.h.brown@gmail.com>
2024-12-02 10:11:02 -0500

    Add Day 2 - Red Nosed Reports

---
+++ Day-02/day_02.rb
@@ -0,0 +1,129 @@
+# Day 2 - Red Nosed Reports
+#
+# This class provides methods to inspect reports and determine if they are safe or unsafe.
+class RedNosedReports
+
+  # Initializes the RedNosedReports class.
+  #
+  # @return [nil]
+  def initialize
+    @reports = []
+    @safe_reports = []
+    @unsafe_reports = []
+  end
+
+  # Adds a report to the reports array.
+  #
+  # @param report [Array] The report to add.
+  # @return [nil]
+  def add_report(report)
+    @reports << report
+  end
+
+  # Clears the safe and unsafe reports arrays, then inspects each report to determine if it is safe or unsafe.
+  # Loads the safe reports into the @safe_reports array and the unsafe reports into the @unsafe_reports array.
+  #
+  # @return [nil]
+  def load_initial_reports
+    @safe_reports = []
+    @unsafe_reports = []
+    @reports.each do |report|
+      safe = inspect_report(report)
+      safe ? @safe_reports << report : @unsafe_reports << report
+    end
+  end
+
+  # Inspects a report to determine if it is safe or unsafe.
+  # A report is considered safe if:
+  # - The numbers in the report are in ascending or descending order.
+  # - The difference between any two numbers in the report is less than or equal to 3, and greater than or equal to 1.
+  #
+  # @param report [Array] The report to inspect.
+  # @return [Boolean] True if the report is safe, false if it is unsafe.
+  def inspect_report(report)
+    directions = []
+    level_differences = []
+
+    report.each_with_index do |number, index|
+      if index > 0
+        if number < report[index - 1]
+          directions << -1
+        elsif number > report[index - 1]
+          directions << 1
+        elsif number == report[index - 1]
+          directions << 0
+        end
+
+        level_differences << (number - report[index - 1]).abs
+      end
+    end
+    directions.uniq!
+
+    return false if directions.length != 1
+    return false if level_differences.max > 3
+    return false if level_differences.min < 1
+    true
+  end
+
+  # Dampens the unsafe reports by removing one number from each report and checking if the report is made safe.
+  # If the report is now safe, it is added to the safe reports array.
+  #
+  # @return [nil]
+  def dampen_unsafe_reports
+    @unsafe_reports.each do |report|
+      report.each_with_index do |number, index|
+        new_report = report.dup
+        new_report.delete_at(index)
+        safe = inspect_report(new_report)
+        if safe
+          @safe_reports << new_report
+          break
+        end
+      end
+    end
+  end
+
+  # Returns the number of safe reports.
+  #
+  # @return [Integer] The number of safe reports.
+  def safe_report_count
+    @safe_reports.length
+  end
+
+  # Reports the number of safe reports as a string.
+  #
+  # @return [nil]
+  def report_safe_report_count
+    puts "Safe reports: #{safe_report_count}"
+  end
+
+  # Loads the input file, and adds each report to the reports array.
+  #
+  # @param input_file [String] The name of the input file. Expected to be in the same directory as this file.
+  # @return [nil]
+  def load_input(input_file)
+    @reports = []
+    file = File.open(File.expand_path(input_file, __dir__), "r")
+    file.readlines.each do |line|
+      if line.length > 2
+        split_line = line.split(" ")
+        report = []
+        split_line.each do |number|
+          report << number.to_i
+        end
+        add_report(report)
+      end
+    end
+  end
+
+end
+
+# Example Usage
+if __FILE__ == $0
+  red_nosed_reports = RedNosedReports.new
+  red_nosed_reports.load_input("input.txt")
+  red_nosed_reports.load_initial_reports
+  red_nosed_reports.report_safe_report_count
+  red_nosed_reports.dampen_unsafe_reports
+  red_nosed_reports.report_safe_report_count
+end
---
+++ spec/day_02_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../Day-02/day_02'
+
+RSpec.describe RedNosedReports do
+  solver = RedNosedReports.new
+  solver.add_report([7,6,4,2,1])
+  solver.add_report([1,2,7,8,9])
+  solver.add_report([9,7,6,2,1])
+  solver.add_report([1,3,2,4,5])
+  solver.add_report([8,6,4,4,1])
+  solver.add_report([1,3,6,7,9])
+
+  solver.load_initial_reports
+
+  it 'finds the correct number of safe reports' do
+    expect(solver.safe_report_count).to eq(2)
+  end
+
+  it 'dampens the correct number of reports' do
+    solver.dampen_unsafe_reports
+    expect(solver.safe_report_count).to eq(4)
+  end
+
+end
