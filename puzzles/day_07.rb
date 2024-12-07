b7581673075ce51aa65fe451b0f3d9360b4b0c0c
PanoramicPanda <uriah.h.brown@gmail.com>
2024-12-07 07:48:50 -0500

    Day 07 solve

---
+++ puzzles/day_07.rb
@@ -0,0 +1,91 @@
+require_relative '../utilities/advent_helpers'
+
+# Day 7: Bridge Repair
+# Tracks the numbers in a calibration and determines if they can be evaluated to match the calibration.
+class BridgeRepair
+
+  attr_reader :calibrations
+  def initialize
+    @operators = ['+','*']
+    @calibrations = {}
+  end
+
+  def load_numbers(line)
+    line_split = line.split(':')
+    @calibrations[line_split[0].to_i] = line_split[1].strip.split(' ').map(&:to_i)
+  end
+
+  # Given a calibration, determine if it can be evaluated.
+  # A calibration can be evaluated if, from left to right,
+  # numbers can utilize any combination of operators to equal the calibration.
+  # We always evaluate from left to right.
+  #
+  # For example, given the calibration 190: 10 19
+  # We first evaluate 10 + 19 = 29, which is not equal to 190.
+  # We then evaluate 10 * 19 = 190, which is equal to 190.
+  # Therefore, it's true.
+  #
+  # Given the calibration 3267: 81 40 27
+  # We first evaluate 81 + 40 + 27 = 148, which is not equal to 3267.
+  # We then evaluate 81 * 40 * 27 = 87120, which is not equal to 3267.
+  # We then evaluate 81 * 40 + 27 = 3267, which is equal to 3267.
+  # Therefore, it's true.
+  #
+  # Given the calibration 161011: 16 10 13
+  # We first evaluate 16 + 10 + 13 = 39, which is not equal to 161011.
+  # We then evaluate 16 * 10 * 13 = 2080, which is not equal to 161011.
+  # We then evaluate 16 * 10 + 13 = 173, which is not equal to 161011.
+  # We then evaluate 16 + 10 * 13 = 338, which is not equal to 161011.
+  # Since we've exhausted all possible combinations, it's false.
+  #
+  # @param calibration [Integer] The calibration to evaluate.
+  # @param concat [Boolean] True if we should try to concatenate the numbers as an operation, false otherwise.
+  #
+  # @return [Boolean] True if the calibration can be evaluated, false otherwise.
+  def can_eval?(calibration, concat: false)
+    operators = concat ? @operators + ['||'] : @operators
+    operator_combinations = operators.repeated_permutation(@calibrations[calibration].length - 1).to_a
+    operator_combinations.each_with_index do |operator, index|
+      result = @calibrations[calibration][0]
+      @calibrations[calibration].each_with_index do |num, num_index|
+        next if num_index == 0
+        if operator[num_index - 1] == '||'
+          result = (result.to_s + num.to_s).to_i
+        else
+          result = result.send(operator[num_index - 1], num)
+        end
+      end
+      return true if result == calibration
+    end
+    false
+  end
+
+  # Sums the correct evaluations.
+  # For each loaded calibration, if it can be evaluated, add it to the sum.
+  #
+  # @param concat [Boolean] True if we should try to concatenate the numbers as an operation, false otherwise.
+  #
+  # @return [Integer] The sum of the correct evaluations.
+  def sum_correct_evaluations(concat: false)
+    @calibrations.select { |calibration, _| can_eval?(calibration, concat: concat) }.keys.sum
+  end
+
+  # Loads the input file and processes each line.
+  #
+  # @param input [String] The name of the file to load.
+  #
+  # @return [nil]
+  def load_input(input)
+    AdventHelpers.load_file_and_do(input) do |line|
+      load_numbers(line)
+    end
+  end
+end
+
+# Example usage
+if __FILE__ == $PROGRAM_NAME
+  solver = BridgeRepair.new
+  solver.load_input('day_07.txt')
+  puts "Sum of correct evaluations: #{solver.sum_correct_evaluations}"
+  puts "Sum of correct evaluations with concatenation: #{solver.sum_correct_evaluations(concat: true)}"
+end
---
+++ spec/day_07_spec.rb
@@ -0,0 +1,43 @@
+require_relative '../puzzles/day_07'
+
+RSpec.describe BridgeRepair do
+
+  sample_input = <<-SAMPLE_INPUT
+    190: 10 19
+    3267: 81 40 27
+    83: 17 5
+    156: 15 6
+    7290: 6 8 6 15
+    161011: 16 10 13
+    192: 17 8 14
+    21037: 9 7 18 13
+    292: 11 6 16 20
+  SAMPLE_INPUT
+  solver = BridgeRepair.new
+  sample_input.each_line do |line|
+    solver.load_numbers(line)
+  end
+
+
+  it 'loads the numbers correctly' do
+    expect(solver.calibrations[190]).to eq([10,19])
+  end
+
+  it 'determines if it can be evaluated correctly' do
+    expect(solver.can_eval?(190)).to eq(true)
+    expect(solver.can_eval?(83)).to eq(false)
+  end
+
+  it 'sums the correct evaluations correctly' do
+    expect(solver.sum_correct_evaluations).to eq(3749)
+  end
+
+  it 'performs concatenation correctly' do
+    expect(solver.can_eval?(156, concat: true)).to eq(true)
+  end
+
+  it 'sums the correct evaluations with concatenation correctly' do
+    expect(solver.sum_correct_evaluations(concat: true)).to eq(11387)
+  end
+
+end
